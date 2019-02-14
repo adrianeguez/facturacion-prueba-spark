8d7061a55bc9e3a192b7ea5fea7c7f24735580e6
Adrian Eguez <eadepto@hotmail.com>
2019-02-13 20:25:41 -0500

    Anadir librearias

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,8 @@ import spark.kotlin.*
 
 fun main(args: Array<String>) {
     val http: Http = ignite()
-    val a = Main()
 
     http.get("/hello") {
-        a.hello()
         return@get "Hola"
     }
 }
